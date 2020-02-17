ec4c7471b778e3df96f34f86a62e41d209fefc3b
mk <33376471+null096@users.noreply.github.com>
2020-02-17 23:46:12 +0200

    use Proxy to wrap global proxy

--- pure/index.js
+++ pure/index.js
@@ -3,17 +3,21 @@
 var proxiesSet = new WeakSet();
 var GlobalProxy = Proxy;
 
-exports.Proxy = function(target, handler) {
-  var proxy = new GlobalProxy(target, handler);
-  proxiesSet.add(proxy);
-  return proxy;
-};
+exports.Proxy = new GlobalProxy(GlobalProxy, {
+  construct(target, args) {
+    var proxy = Reflect.construct(target, args);
+    proxiesSet.add(proxy);
+    return proxy;
+  }
+});
 
-exports.Proxy.revocable = function(target, handler) {
-  var proxyObj = GlobalProxy.revocable(target, handler);
-  proxiesSet.add(proxyObj.proxy);
-  return proxyObj;
-};
+exports.Proxy.revocable = new GlobalProxy(GlobalProxy.revocable, {
+  apply(target, thisArg, argumentsList) {
+    var proxyObj = Reflect.apply(target, thisArg, argumentsList);
+    proxiesSet.add(proxyObj.proxy);
+    return proxyObj;  
+  }
+});
 
 /**
  * Checks whether a value is a proxy object
